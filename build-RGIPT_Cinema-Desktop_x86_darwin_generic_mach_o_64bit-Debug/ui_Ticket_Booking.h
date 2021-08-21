115cdba2a739eb7439df615201161f083ab5485b
advait gupta <advaitgupta@ymail.com>
2021-08-21 21:45:51 +0530

    rectified typo

---
+++ build-RGIPT_Cinema-Desktop_x86_darwin_generic_mach_o_64bit-Debug/ui_Ticket_Booking.h
@@ -0,0 +1,284 @@
+/********************************************************************************
+** Form generated from reading UI file 'Ticket_Booking.ui'
+**
+** Created by: Qt User Interface Compiler version 6.1.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_TICKET_BOOKING_H
+#define UI_TICKET_BOOKING_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_thirdDialog
+{
+public:
+    QLabel *label;
+    QLabel *label_2;
+    QCheckBox *A1;
+    QCheckBox *A2;
+    QCheckBox *A3;
+    QCheckBox *B1;
+    QCheckBox *A5;
+    QCheckBox *A4;
+    QCheckBox *B2;
+    QCheckBox *B3;
+    QCheckBox *B4;
+    QCheckBox *B5;
+    QCheckBox *C1;
+    QCheckBox *C2;
+    QCheckBox *C3;
+    QCheckBox *C4;
+    QCheckBox *C5;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QComboBox *time;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QLabel *label_6;
+    QLabel *label_7;
+    QLabel *label_8;
+
+    void setupUi(QDialog *thirdDialog)
+    {
+        if (thirdDialog->objectName().isEmpty())
+            thirdDialog->setObjectName(QString::fromUtf8("thirdDialog"));
+        thirdDialog->resize(621, 520);
+        thirdDialog->setStyleSheet(QString::fromUtf8("QPushButton#pushButton{\n"
+"	\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"color:rgpa(255,255,255,210);\n"
+"border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton:hover{\n"
+"	background-color: qlineargradient(spread:pad, x1:0.0832857, y1:0.392, x2:1, y2:1, stop:0.309091 rgba(176, 190, 197,255), stop:0.981061 rgba(246, 186, 156, 255));\n"
+"	\n"
+"}\n"
+"QPushButton#pushButton:pressed{\n"
+"padding-left:5px;\n"
+"padding-top:5px;\n"
+"background-color:rgba(150,123,111,225);\n"
+"}\n"
+"QPushButton#pushButton_2{\n"
+"	\n"
+"	\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"color:rgpa(255,255,255,210);\n"
+"border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton_2:hover{\n"
+"	background-color: qlineargradient(spread:pad, x1:0.0832857, y1:0.392, x2:1, y2:1, stop:0.309091 rgba(176, 190, 197,2"
+                        "55), stop:0.981061 rgba(246, 186, 156, 255));\n"
+"	\n"
+"}\n"
+"QPushButton#pushButton_2:pressed{\n"
+"padding-left:5px;\n"
+"padding-top:5px;\n"
+"background-color:rgba(150,123,111,225);\n"
+"}"));
+        label = new QLabel(thirdDialog);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(20, 20, 580, 480));
+        label->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/img3.png);\n"
+"\n"
+"border-top-left-radius:50px;\n"
+"border-bottom-right-radius:50px;"));
+        label_2 = new QLabel(thirdDialog);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(80, 40, 460, 40));
+        QFont font;
+        font.setPointSize(10);
+        font.setBold(true);
+        label_2->setFont(font);
+        label_2->setStyleSheet(QString::fromUtf8("background-color: rgb(187, 203, 213);\n"
+"border-top-left-radius:20px;\n"
+"border-bottom-right-radius:20px;\n"
+""));
+        A1 = new QCheckBox(thirdDialog);
+        A1->setObjectName(QString::fromUtf8("A1"));
+        A1->setGeometry(QRect(90, 115, 50, 50));
+        QFont font1;
+        font1.setPointSize(8);
+        font1.setBold(true);
+        A1->setFont(font1);
+        A1->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);\n"
+"color: rgb(255, 255, 255);"));
+        A2 = new QCheckBox(thirdDialog);
+        A2->setObjectName(QString::fromUtf8("A2"));
+        A2->setGeometry(QRect(190, 115, 50, 50));
+        A2->setFont(font1);
+        A2->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);\n"
+"color: rgb(255, 255, 255);"));
+        A3 = new QCheckBox(thirdDialog);
+        A3->setObjectName(QString::fromUtf8("A3"));
+        A3->setGeometry(QRect(290, 115, 50, 50));
+        QFont font2;
+        font2.setBold(true);
+        A3->setFont(font2);
+        A3->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);\n"
+"color: rgb(255, 255, 255);"));
+        B1 = new QCheckBox(thirdDialog);
+        B1->setObjectName(QString::fromUtf8("B1"));
+        B1->setGeometry(QRect(90, 210, 50, 50));
+        B1->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        A5 = new QCheckBox(thirdDialog);
+        A5->setObjectName(QString::fromUtf8("A5"));
+        A5->setGeometry(QRect(490, 115, 50, 50));
+        A5->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        A4 = new QCheckBox(thirdDialog);
+        A4->setObjectName(QString::fromUtf8("A4"));
+        A4->setGeometry(QRect(390, 115, 50, 50));
+        A4->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        B2 = new QCheckBox(thirdDialog);
+        B2->setObjectName(QString::fromUtf8("B2"));
+        B2->setGeometry(QRect(190, 210, 50, 50));
+        B2->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        B3 = new QCheckBox(thirdDialog);
+        B3->setObjectName(QString::fromUtf8("B3"));
+        B3->setGeometry(QRect(290, 210, 50, 50));
+        B3->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        B4 = new QCheckBox(thirdDialog);
+        B4->setObjectName(QString::fromUtf8("B4"));
+        B4->setGeometry(QRect(390, 210, 50, 50));
+        B4->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        B5 = new QCheckBox(thirdDialog);
+        B5->setObjectName(QString::fromUtf8("B5"));
+        B5->setGeometry(QRect(490, 210, 50, 50));
+        B5->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        C1 = new QCheckBox(thirdDialog);
+        C1->setObjectName(QString::fromUtf8("C1"));
+        C1->setGeometry(QRect(90, 305, 50, 50));
+        C1->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        C2 = new QCheckBox(thirdDialog);
+        C2->setObjectName(QString::fromUtf8("C2"));
+        C2->setGeometry(QRect(190, 305, 50, 50));
+        C2->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        C3 = new QCheckBox(thirdDialog);
+        C3->setObjectName(QString::fromUtf8("C3"));
+        C3->setGeometry(QRect(290, 305, 50, 50));
+        C3->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        C4 = new QCheckBox(thirdDialog);
+        C4->setObjectName(QString::fromUtf8("C4"));
+        C4->setGeometry(QRect(390, 305, 50, 50));
+        C4->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        C5 = new QCheckBox(thirdDialog);
+        C5->setObjectName(QString::fromUtf8("C5"));
+        C5->setGeometry(QRect(490, 305, 50, 50));
+        C5->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/3453496.png);"));
+        label_3 = new QLabel(thirdDialog);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(250, 90, 121, 21));
+        QFont font3;
+        font3.setFamilies({QString::fromUtf8("Arial")});
+        font3.setPointSize(9);
+        label_3->setFont(font3);
+        label_3->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);\n"
+"border-bottom:0.2px solid rgb(255,255,255);"));
+        label_4 = new QLabel(thirdDialog);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(230, 180, 161, 20));
+        label_4->setFont(font3);
+        label_4->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_5 = new QLabel(thirdDialog);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(250, 280, 121, 20));
+        label_5->setFont(font3);
+        label_5->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        time = new QComboBox(thirdDialog);
+        time->addItem(QString());
+        time->addItem(QString());
+        time->addItem(QString());
+        time->setObjectName(QString::fromUtf8("time"));
+        time->setGeometry(QRect(120, 385, 110, 30));
+        QFont font4;
+        font4.setPointSize(11);
+        font4.setBold(true);
+        time->setFont(font4);
+        time->setStyleSheet(QString::fromUtf8("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"border-radius:5px;"));
+        pushButton = new QPushButton(thirdDialog);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(270, 385, 90, 30));
+        pushButton->setFont(font4);
+        pushButton->setStyleSheet(QString::fromUtf8(""));
+        pushButton_2 = new QPushButton(thirdDialog);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(400, 385, 80, 30));
+        pushButton_2->setFont(font4);
+        label_6 = new QLabel(thirdDialog);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setGeometry(QRect(550, 130, 47, 13));
+        QFont font5;
+        font5.setPointSize(9);
+        font5.setBold(true);
+        label_6->setFont(font5);
+        label_6->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_7 = new QLabel(thirdDialog);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+        label_7->setGeometry(QRect(550, 230, 47, 13));
+        label_7->setFont(font5);
+        label_7->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_8 = new QLabel(thirdDialog);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+        label_8->setGeometry(QRect(550, 320, 47, 13));
+        label_8->setFont(font5);
+        label_8->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+
+        retranslateUi(thirdDialog);
+
+        QMetaObject::connectSlotsByName(thirdDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *thirdDialog)
+    {
+        thirdDialog->setWindowTitle(QCoreApplication::translate("thirdDialog", "Dialog", nullptr));
+        label->setText(QString());
+        label_2->setText(QCoreApplication::translate("thirdDialog", "                                                     SCREEN", nullptr));
+        A1->setText(QString());
+        A2->setText(QString());
+        A3->setText(QString());
+        B1->setText(QString());
+        A5->setText(QString());
+        A4->setText(QString());
+        B2->setText(QString());
+        B3->setText(QString());
+        B4->setText(QString());
+        B5->setText(QString());
+        C1->setText(QString());
+        C2->setText(QString());
+        C3->setText(QString());
+        C4->setText(QString());
+        C5->setText(QString());
+        label_3->setText(QCoreApplication::translate("thirdDialog", "\342\206\223 CLASSIC( \342\202\271 99.00) \342\206\223", nullptr));
+        label_4->setText(QCoreApplication::translate("thirdDialog", "\342\206\223 CLASSIC PLUS( \342\202\271 112.00) \342\206\223", nullptr));
+        label_5->setText(QCoreApplication::translate("thirdDialog", "\342\206\223 PRIME( \342\202\271 149.00) \342\206\223", nullptr));
+        time->setItemText(0, QCoreApplication::translate("thirdDialog", "Select Time", nullptr));
+        time->setItemText(1, QCoreApplication::translate("thirdDialog", "10:00 AM", nullptr));
+        time->setItemText(2, QCoreApplication::translate("thirdDialog", "03:00 PM", nullptr));
+
+        pushButton->setText(QCoreApplication::translate("thirdDialog", "Book/Pay", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("thirdDialog", "Close", nullptr));
+        label_6->setText(QCoreApplication::translate("thirdDialog", "A1-A5", nullptr));
+        label_7->setText(QCoreApplication::translate("thirdDialog", "B1-B5", nullptr));
+        label_8->setText(QCoreApplication::translate("thirdDialog", "C1-C5", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class thirdDialog: public Ui_thirdDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_TICKET_BOOKING_H
