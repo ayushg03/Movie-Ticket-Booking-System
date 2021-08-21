f106305c0bdc8fb9d9d12ceba5b84ca0f460d7e0
advait gupta <advaitgupta@ymail.com>
2021-08-21 21:51:24 +0530

    changed location of the images folder since q resource expects it insed Final-Project folder

---
+++ build-RGIPT_Cinema-Desktop_x86_darwin_generic_mach_o_64bit-Debug/ui_Movies.h
@@ -0,0 +1,268 @@
+/********************************************************************************
+** Form generated from reading UI file 'Movies.ui'
+**
+** Created by: Qt User Interface Compiler version 6.1.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MOVIES_H
+#define UI_MOVIES_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_SecDialog
+{
+public:
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLabel *label_9;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_6;
+    QLabel *label_5;
+    QLabel *label_6;
+    QLabel *label_10;
+
+    void setupUi(QDialog *SecDialog)
+    {
+        if (SecDialog->objectName().isEmpty())
+            SecDialog->setObjectName(QString::fromUtf8("SecDialog"));
+        SecDialog->resize(621, 520);
+        SecDialog->setStyleSheet(QString::fromUtf8("QPushButton#pushButton{\n"
+"	\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"color:rgpa(255,255,255,210);\n"
+"border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton:hover{\n"
+"	background-color: qlineargradient(spread:pad, x1:0.0832857, y1:0.392, x2:1, y2:1, stop:0.309091 rgba(176, 190, 197,255), stop:0.981061 rgba(246, 186, 156, 255));\n"
+"	\n"
+"}\n"
+"QPushButton#pushButton:pressed{\n"
+"padding-left:5px;\n"
+"padding-top:5px;\n"
+"background-color:rgba(150,123,111,225);\n"
+"}\n"
+"\n"
+"QPushButton#pushButton_2{\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"color:rgpa(255,255,255,210);\n"
+"border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton_2:hover{\n"
+"	background-color: qlineargradient(spread:pad, x1:0.0832857, y1:0.392, x2:1, y2:1, stop:0.309091 rgba(176, 190, 197,255), st"
+                        "op:0.981061 rgba(246, 186, 156, 255));\n"
+"	\n"
+"}\n"
+"QPushButton#pushButton_2:pressed{\n"
+"padding-left:5px;\n"
+"padding-top:5px;\n"
+"background-color:rgba(150,123,111,225);\n"
+"}\n"
+"QPushButton#pushButton_3{\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"color:rgpa(255,255,255,210);\n"
+"border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton_3:hover{\n"
+"	background-color: qlineargradient(spread:pad, x1:0.0832857, y1:0.392, x2:1, y2:1, stop:0.309091 rgba(176, 190, 197,255), stop:0.981061 rgba(246, 186, 156, 255));\n"
+"	\n"
+"}\n"
+"QPushButton#pushButton_3:pressed{\n"
+"padding-left:5px;\n"
+"padding-top:5px;\n"
+"background-color:rgba(150,123,111,225);\n"
+"}\n"
+"QPushButton#pushButton_4{\n"
+"	\n"
+"	\n"
+"	\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"color:rgpa(255,255,255,"
+                        "210);\n"
+"border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton_4:hover{\n"
+"	background-color: qlineargradient(spread:pad, x1:0.0832857, y1:0.392, x2:1, y2:1, stop:0.309091 rgba(176, 190, 197,255), stop:0.981061 rgba(246, 186, 156, 255));\n"
+"	\n"
+"}\n"
+"QPushButton#pushButton_4:pressed{\n"
+"padding-left:5px;\n"
+"padding-top:5px;\n"
+"background-color:rgba(150,123,111,225);\n"
+"}\n"
+"QPushButton#pushButton_5{\n"
+"	\n"
+"	\n"
+"	\n"
+"	\n"
+"	\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"color:rgpa(255,255,255,210);\n"
+"border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton_5:hover{\n"
+"	background-color: qlineargradient(spread:pad, x1:0.0832857, y1:0.392, x2:1, y2:1, stop:0.309091 rgba(176, 190, 197,255), stop:0.981061 rgba(246, 186, 156, 255));\n"
+"	\n"
+"}\n"
+"QPushButton#pushButton_5:pressed{\n"
+"padding-left:5px;\n"
+"padding-top:5px;\n"
+"background-color:rgba(150,123,111,225);\n"
+"}\n"
+"QPushBu"
+                        "tton#pushButton_6{\n"
+"	\n"
+"	\n"
+"	\n"
+"	\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.438, y2:0.579545, stop:0.176136 rgba(255, 205, 102, 255), stop:0.585227 rgba(253, 190, 0, 255));\n"
+"color:rgpa(255,255,255,210);\n"
+"border-radius:5px;\n"
+"}\n"
+"QPushButton#pushButton_6:hover{\n"
+"	background-color: qlineargradient(spread:pad, x1:0.0832857, y1:0.392, x2:1, y2:1, stop:0.309091 rgba(176, 190, 197,255), stop:0.981061 rgba(246, 186, 156, 255));\n"
+"	\n"
+"}\n"
+"QPushButton#pushButton_6:pressed{\n"
+"padding-left:5px;\n"
+"padding-top:5px;\n"
+"background-color:rgba(150,123,111,225);\n"
+"}"));
+        label = new QLabel(SecDialog);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(20, 20, 580, 480));
+        label->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/img3.png);\n"
+"border-top-left-radius:50px;\n"
+"border-bottom-right-radius:50px;\n"
+""));
+        label_2 = new QLabel(SecDialog);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(30, 90, 181, 341));
+        label_2->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);\n"
+"border-top-left-radius:20px;\n"
+"border-bottom-right-radius:20px;\n"
+"\n"
+""));
+        label_3 = new QLabel(SecDialog);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(220, 90, 181, 341));
+        label_3->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);\n"
+"border-top-left-radius:20px;\n"
+"border-bottom-right-radius:20px;"));
+        label_4 = new QLabel(SecDialog);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(410, 90, 181, 341));
+        label_4->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);\n"
+"border-top-left-radius:20px;\n"
+"border-bottom-right-radius:20px;"));
+        label_7 = new QLabel(SecDialog);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+        label_7->setGeometry(QRect(40, 100, 161, 191));
+        label_7->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/movie1.jfif);\n"
+"border-top-left-radius:10px;\n"
+"border-bottom-right-radius:10px;"));
+        label_8 = new QLabel(SecDialog);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+        label_8->setGeometry(QRect(230, 100, 161, 191));
+        label_8->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/poster-780.jpg);\n"
+"border-top-left-radius:10px;\n"
+"border-bottom-right-radius:10px;"));
+        label_9 = new QLabel(SecDialog);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+        label_9->setGeometry(QRect(420, 100, 161, 191));
+        label_9->setStyleSheet(QString::fromUtf8("border-image: url(:/img/images/The_Suicide_Squad_official_poster.jpeg);\n"
+"border-top-left-radius:10px;\n"
+"border-bottom-right-radius:10px;"));
+        pushButton = new QPushButton(SecDialog);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(60, 340, 121, 31));
+        QFont font;
+        font.setPointSize(11);
+        font.setBold(true);
+        pushButton->setFont(font);
+        pushButton_2 = new QPushButton(SecDialog);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(250, 340, 121, 31));
+        pushButton_2->setFont(font);
+        pushButton_3 = new QPushButton(SecDialog);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(440, 340, 121, 31));
+        pushButton_3->setFont(font);
+        pushButton_4 = new QPushButton(SecDialog);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(60, 380, 121, 31));
+        pushButton_4->setFont(font);
+        pushButton_5 = new QPushButton(SecDialog);
+        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
+        pushButton_5->setGeometry(QRect(250, 380, 121, 31));
+        pushButton_5->setFont(font);
+        pushButton_6 = new QPushButton(SecDialog);
+        pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
+        pushButton_6->setGeometry(QRect(440, 380, 121, 31));
+        pushButton_6->setFont(font);
+        label_5 = new QLabel(SecDialog);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(420, 290, 161, 31));
+        label_5->setStyleSheet(QString::fromUtf8("font: 8pt \"Arial Rounded MT Bold\";"));
+        label_6 = new QLabel(SecDialog);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setGeometry(QRect(230, 290, 161, 31));
+        label_6->setStyleSheet(QString::fromUtf8("font: 8pt \"Arial Rounded MT Bold\";"));
+        label_10 = new QLabel(SecDialog);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+        label_10->setGeometry(QRect(40, 290, 161, 31));
+        label_10->setStyleSheet(QString::fromUtf8("font: 8pt \"Arial Rounded MT Bold\";"));
+
+        retranslateUi(SecDialog);
+
+        QMetaObject::connectSlotsByName(SecDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *SecDialog)
+    {
+        SecDialog->setWindowTitle(QCoreApplication::translate("SecDialog", "Dialog", nullptr));
+        label->setText(QString());
+        label_2->setText(QString());
+        label_3->setText(QString());
+        label_4->setText(QString());
+        label_7->setText(QString());
+        label_8->setText(QString());
+        label_9->setText(QString());
+        pushButton->setText(QCoreApplication::translate("SecDialog", "Book Now", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("SecDialog", "Book Now", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("SecDialog", "Book Now", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("SecDialog", "Watch Trailer", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("SecDialog", "Watch Trailer", nullptr));
+        pushButton_6->setText(QCoreApplication::translate("SecDialog", "Watch Trailer", nullptr));
+        label_5->setText(QCoreApplication::translate("SecDialog", " Censor Rating          Duration\n"
+"  A                                 2h 16m", nullptr));
+        label_6->setText(QCoreApplication::translate("SecDialog", " Censor Rating          Duration\n"
+"  U/A                              2h 56m", nullptr));
+        label_10->setText(QCoreApplication::translate("SecDialog", " Censor Rating      Duration\n"
+"  U/A                          2h 6m", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class SecDialog: public Ui_SecDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MOVIES_H
